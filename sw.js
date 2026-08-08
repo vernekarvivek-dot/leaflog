793e5eaf32ac87a3ce1610887f021aedbe941815
vernekarvivek-dot <vernekarvivek@gmail.com>
2026-08-08 21:11:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ const CACHE_NAME = 'leaflog-cache-v1';
 const ASSETS = [
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-maskable-512.png'
+  './icon/icon-192.png',
+  './icon/icon-512.png',
+  './icon/icon-maskable-512.png'
 ];
 
 self.addEventListener('install', event => {
